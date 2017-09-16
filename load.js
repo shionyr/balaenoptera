ae6e9d603890ce74193bd84bd2043c0fc6f58910
Tom Murray <t.murray@xsolla.com>
2017-09-16 02:02:10 -0700

    Updates

--- load.js
+++ load.js
@@ -1,10 +1,34 @@
 ﻿window.gameData = {
     events: {
-        loadedEvent: (function () { return new Event('loaded'); })()
+        loadingScreenComplete: (function () { return new Event('loadingScreenComplete'); })()
+    },
+    loader: {
+        __iterate: function () {
+            if (document.readyState === 'complete') {
+                clearInterval(this.__interval);
+                this.__onloaded();
+            }
+        },
+        initialize: function () {
+            this.__interval = window.setInterval( function(){
+                window.gameData.loader.__iterate.call(window.gameData.loader);
+            },100);
+        },
+        setOnloaded: function (func) {
+            if (typeof(func) === 'function')
+                this.__onloaded = func;
+        },
+        __onloaded: null,
+        __interval: null
     }
 };
 
-(function () {
-    window.addEventListener('loaded', function () {
+(function (){
+    window.gameData.loader.setOnloaded(function(){
+        document.dispatchEvent(window.gameData.events.loadingScreenComplete);
+        console.log('poop');
     });
+    window.setTimeout(function(){
+        window.gameData.loader.initialize();
+    },500);
 })();
