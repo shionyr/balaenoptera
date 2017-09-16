f2f45e02aa6692535f7ded6d11a2cc02cc5bd6fa
James Gibbs <shionyr@gmail.com>
2017-09-15 23:30:29 -0700

    New first push

---
+++ load.js
@@ -0,0 +1,10 @@
+﻿window.gameData = {
+    events: {
+        loadedEvent: (function () { return new Event('loaded'); })()
+    }
+};
+
+(function () {
+    window.addEventListener('loaded', function () {
+    });
+})();
